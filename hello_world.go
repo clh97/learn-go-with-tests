3d42c9b1447733e427a187854ca58b7ddfa7b669
Michel Calheiros <michel@calheiros.dev>
2021-04-10 03:24:10 -0300

    hello_world refactoring

--- hello_world.go
+++ hello_world.go
@@ -2,10 +2,13 @@ package main
 
 import "fmt"
 
-func Hello() string {
-	return "hello!"
+func Hello(person string) string {
+	if len(person) <= 0 {
+		return "hello, world!"
+	}
+	return fmt.Sprintf("hello, %s!", person)
 }
 
 func main() {
-	fmt.Println("Hello, world")
+	Hello("michel")
 }
