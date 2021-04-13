0f11809a58c1e02c0e8dd6d2edd51371fc15f1e8
Michel Calheiros <michel@calheiros.dev>
2021-04-13 00:47:31 -0300

    added SumAll to arrays_and_slices

--- arrays_and_slices/arrays_and_slices.go
+++ arrays_and_slices/arrays_and_slices.go
@@ -1,8 +1,18 @@
 package arrays_and_slices
 
-func Sum(numbers [5]int) (result int) {
+func Sum(numbers []int) (result int) {
 	for _, num := range numbers {
 		result += num
 	}
 	return result
+}
+
+func SumAll(numbersToSum ...[]int) []int {
+	sums := make([]int, len(numbersToSum))
+
+	for i, numbers := range numbersToSum {
+		sums[i] = Sum(numbers)
+	}
+
+	return sums
 }
--- arrays_and_slices/arrays_and_slices_test.go
+++ arrays_and_slices/arrays_and_slices_test.go
@@ -1,14 +1,41 @@
 package arrays_and_slices
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
 
-	got := Sum(numbers)
-	want := 15
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
 
-	if got != want {
-		t.Errorf("got %d want %d, given %v", got, want, numbers)
+		got := Sum(numbers)
+		want := 15
+
+		if got != want {
+			t.Errorf("got %d want %d, given %v", got, want, numbers)
+		}
+	})
+
+	t.Run("collection of n numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		got := Sum(numbers)
+		want := 6
+
+		if got != want {
+			t.Errorf("got %d want %d, given %v", got, want, numbers)
+		}
+	})
+
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
 	}
 }
