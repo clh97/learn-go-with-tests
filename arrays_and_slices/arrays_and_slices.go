f2fa45181528a206c13091c3e014e8e8c9360d16
Michel Calheiros <michel@calheiros.dev>
2021-04-13 00:38:11 -0300

    refactored arrays_and_slices

--- arrays_and_slices/arrays_and_slices.go
+++ arrays_and_slices/arrays_and_slices.go
@@ -1,8 +1,8 @@
 package arrays_and_slices
 
 func Sum(numbers [5]int) (result int) {
-	for i := 0; i < 5; i++ {
-		result += numbers[i]
+	for _, num := range numbers {
+		result += num
 	}
 	return result
 }
